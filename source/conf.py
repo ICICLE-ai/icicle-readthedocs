9ed38e0898f2871ccff30bd604a4cfd2f2c70ff8
TurtleLions <ryanestanislao@gmail.com>
2023-10-06 18:39:49 -0700

    logo added

--- source/conf.py
+++ source/conf.py
@@ -53,6 +53,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+html_logo = "https://aiinstitutes.org/wp-content/uploads/2022/07/icicle.jpeg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
